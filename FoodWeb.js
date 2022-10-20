a83ca0cd9f2506bc52c9e6186e2a52a8e9b16165
Robert Galbraith <rjgalbra@mtu.edu>
2022-10-20 10:57:51 -0400

    Cleaned up FoodWeb.js

--- FoodWeb.js
+++ FoodWeb.js
@@ -1,23 +1,3 @@
-//10/17/22 code to create a local server on your machine
-
-
-
-// const http = require("http");
-
-// const host = 'localhost';
-// const port = 8000;
-
-// const requestListener = function (req, res){
-// 	res.writeHead(200);
-// 	res.end("Server has been accessed.");
-// };
-
-// const server = http.createServer(requestListener);
-// server.listen(port, host, () => {
-// 	console.log('Server is running on http://${host}:${port}');
-// });
-
-
 // 10/18/2022 MEG: Using express/node to create web application using youtube link 
 // Part I https://www.youtube.com/watch?v=EMwu8F0dCXE&ab_channel=rdzfinalrounduser
 // Part II https://www.youtube.com/watch?v=rt9AS79ppnM&ab_channel=rdzfinalrounduser
