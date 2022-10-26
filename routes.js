7bfcb1c143d59fc9365bc60ff47883222f7218e1
megalbra <megalbra@mtu.edu>
2022-10-25 22:09:24 -0400

    Linked paged to their views and added buttons

--- routes.js
+++ routes.js
@@ -2,10 +2,36 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/", function(req,res)
+router.get("/index", function(req,res)
 {
     console.log("Hello Im on the start page!");
     res.render("index"); //
 });
+
+router.get("/FAQ", function(req,res)
+{
+    res.render("FAQ");
+});
+
+router.get("/Log_In", function(req,res)
+{
+    res.render("Log_In");
+});
+
+router.get("/Creators", function(req,res)
+{
+    res.render("Creators");
+});
+
+router.get("/Learn_App", function(req,res)
+{
+    res.render("Learn_App");
+});
+
+router.get("/Find_Recipe", function(req,res)
+{
+    res.render("Find_Recipe");
+});
+
 
 module.exports = router; // need to export to use in other modules
