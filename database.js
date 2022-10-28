e63641a21ae0ec98ff209435f84c80bacff65c46
Finley Green <finleyg@mtu.edu>
2022-10-27 20:49:05 -0400

    Database / button things
    + added database connection file
    + added /food_buttons page that lists all of the food items from the database as buttons
    + added /food_list page that shows the foods table for reference

---
+++ database.js
@@ -0,0 +1,22 @@
+// makes connection to mysql database
+// run into errors if in the main app
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'classdb.it.mtu.edu',
+    user: 'cs3141t03r01_rw',
+    password: 'Abc123!!',
+    database: 'cs3141t03r01',
+    port	 :	'3307'
+});
+conn.connect(function(err){
+    if(err){
+		console.log("Ping unsuccessful");
+        console.log("Eror code: " + err.code);
+        console.log("Fatal? " + err.fatal);
+    }
+	else
+	{
+		console.log("Ping successful");
+	}
+});
+module.exports = conn;
--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 var express = require("express");
-
 var router = express.Router();
+const conn = require("./database");
 
 router.get("/", function(req,res)
 {
@@ -38,5 +38,22 @@ router.get("/Find_Recipe", function(req,res)
     res.render("Find_Recipe");
 });
 
+// renders the page with the food table
+router.get('/food_list', function(req, res) {
+    var sql='SELECT * FROM foods';
+    conn.query(sql, function (err, data, fields) {
+        if (err) throw err;
+        res.render('food_list', { title: 'Food List', foodData: data});
+  });
+});
+
+// renders a page with food buttons (no filters for category implemented yet)
+router.get('/food_buttons', function(req, res) {
+    var sql='SELECT * FROM foods';
+    conn.query(sql, function (err, data, fields) {
+        if (err) throw err;
+        res.render('food_buttons', { title: 'Food Buttons', foodData: data});
+  });
+});
 
 module.exports = router; // need to export to use in other modules
