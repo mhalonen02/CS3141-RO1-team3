2ff021b91059fc5b1e0c7fec78edaa38473e9e72
Robert Galbraith <rjgalbra@mtu.edu>
2022-11-02 12:55:58 -0400

    Able to query results to console log

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 // makes connection to mysql database
 // run into errors if in the main app
 var mysql = require('mysql');
+
 var conn = mysql.createConnection({ 
     multipleStatements: true,
     host: 'classdb.it.mtu.edu',
@@ -9,6 +10,7 @@ var conn = mysql.createConnection({
     database: 'cs3141t03r01',
     port	 :	'3307'
 });
+
 conn.connect(function(err){
     if(err){
 		console.log("Ping unsuccessful");
@@ -19,6 +21,17 @@ conn.connect(function(err){
 	{
 		console.log("Ping successful");
 	}
+});
+
+$query_1 = 'SELECT * from foodCategory LIMIT 10';
+
+conn.query($query_1, function(err, rows) {
+    if(err){
+        console.log("An error ocurred performing the query.");
+        return;
+    }
+
+    console.log("Query succesfully executed: ", rows);
 });
 
 // query function to run sql statements - NOT currently working
